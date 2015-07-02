15fff41f93c6483be67faadb588d276f65317016
Marcin Kolny <marcin.kolny@gmail.com>
2015-07-03 00:21:25 +0200

    gst-debugserver: g_flags support in gvalue_enum wrapper

--- src/gst-debugger/gvalue-converter/gvalue_enum.cpp
+++ src/gst-debugger/gvalue-converter/gvalue_enum.cpp
@@ -13,25 +13,47 @@ GValueEnum::GValueEnum(GValue *gobj)
 : GValueBase(gobj)
 {}
 
-std::string GValueEnum::to_string() const
+template<typename T>
+void append_to_vector(std::vector<std::pair<int, std::string>>& values, T *enum_values)
 {
-	Glib::Value_Enum<int> v;
-	v.init(g_value);
-	GEnumValue *values = G_ENUM_CLASS (g_type_class_ref(G_VALUE_TYPE(g_value)))->values;
-
-	return values[v.get()].value_nick;
-}
-
-std::vector<std::pair<int, std::string>> GValueEnum::get_values(GType type)
-{
-	std::vector<std::pair<int, std::string>> values;
-	GEnumValue *enum_values = G_ENUM_CLASS (g_type_class_ref(type))->values;
 	int i = 0;
 
 	while (enum_values[i].value_name)
 	{
 		values.push_back(std::make_pair(enum_values[i].value, enum_values[i].value_nick));
 		i++;
+	}
+}
+
+std::string GValueEnum::to_string() const
+{
+	gpointer ptr = g_type_class_ref(G_VALUE_TYPE(g_value));
+	if (G_IS_ENUM_CLASS(ptr))
+	{
+		Glib::Value_Enum<int> v;
+		v.init(g_value);
+		return G_ENUM_CLASS (ptr)->values[v.get()].value_nick;
+	}
+	else
+	{
+		Glib::Value_Flags<int> v;
+		v.init(g_value);
+		return G_FLAGS_CLASS (ptr)->values[v.get()].value_nick;
+	}
+}
+
+std::vector<std::pair<int, std::string>> GValueEnum::get_values(GType type)
+{
+	std::vector<std::pair<int, std::string>> values;
+	gpointer ptr = g_type_class_ref(type);
+
+	if (G_IS_ENUM_CLASS (ptr))
+	{
+		append_to_vector(values, G_ENUM_CLASS (ptr)->values);
+	}
+	else
+	{
+		append_to_vector(values, G_FLAGS_CLASS (ptr)->values);
 	}
 
 	return values;
